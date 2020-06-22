2a4ccfb1a98f1f0bc50bd4f5e5f5fe18b6b2c25b
Tkacha <tanoo.kacha@g.swu.ac.th>
2020-06-22 22:42:16 +0700

    edit config nodejs error user vagrant

--- run-project.sh
+++ run-project.sh
@@ -7,12 +7,12 @@ docker-compose -f docker-compose.yml up -d --build
 echo "--------------------------------------"
 echo " 📦 Install prerequisites"
 echo "--------------------------------------"
-docker exec -it sandbox_node_1 bash -c "make install"
+docker exec -it nodejs bash -c "make install"
 
 echo "--------------------------------------"
 echo " 🐳 Restarting Node container ..."
 echo "--------------------------------------"
-docker restart sandbox_node_1
+docker restart nodejs
 # Not the best approach but pending until I could research for the better solution
 
 echo "--------------------------------------"
