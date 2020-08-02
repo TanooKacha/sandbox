3e7e908a29dd614d8ab6a92be49909a26786866c
Tkacha <tanoo.kacha@g.swu.ac.th>
2020-08-02 17:48:33 +0700

    edit run

--- run-project.sh
+++ run-project.sh
@@ -1,3 +1,4 @@
+  
 #!/bin/bash
 echo "--------------------------------------"
 echo " 🐙 Docker compose ..."
@@ -7,12 +8,13 @@ docker-compose -f docker-compose.yml up -d --build
 echo "--------------------------------------"
 echo " 📦 Install prerequisites"
 echo "--------------------------------------"
-docker exec -it nodejs bash -c "make install"
+docker exec -it node01 bash -c "make install"
+docker exec -it node02 bash -c "make install"
 
 echo "--------------------------------------"
 echo " 🐳 Restarting Node container ..."
 echo "--------------------------------------"
-docker restart nodejs
+docker restart node01 node02
 # Not the best approach but pending until I could research for the better solution
 
 echo "--------------------------------------"
