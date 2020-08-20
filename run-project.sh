ebe466688cb5d3b45d1ea2c0a9b120a30cde0ec8
Tkacha <tanoo.kacha@g.swu.ac.th>
2020-08-20 21:05:11 +0700

    Add docker exec node 00-03

--- run-project.sh
+++ run-project.sh
@@ -8,13 +8,15 @@ docker-compose -f docker-compose.yml up -d --build
 echo "--------------------------------------"
 echo " 📦 Install prerequisites"
 echo "--------------------------------------"
+docker exec -it node00 bash -c "make install"
 docker exec -it node01 bash -c "make install"
 docker exec -it node02 bash -c "make install"
+docker exec -it node03 bash -c "make install"
 
 echo "--------------------------------------"
 echo " 🐳 Restarting Node container ..."
 echo "--------------------------------------"
-docker restart node01 node02
+docker restart node00 node01 node02 node03
 # Not the best approach but pending until I could research for the better solution
 
 echo "--------------------------------------"
